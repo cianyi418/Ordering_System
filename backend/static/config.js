9f3ce578e3927cf1de15fb327c2edcccb7acb1c5
cianyi418 <cianyi418@gmail.com>
2025-05-25 17:45:23 +0800

    Update liffId and orderDetailBaseUrl in APP_CONFIG for correct configuration

--- backend/static/config.js
+++ backend/static/config.js
@@ -1,4 +1,4 @@
 window.APP_CONFIG = {
-  liffId: "2007352498-daVlLJwG",
-  orderDetailBaseUrl: "https://liff.line.me/2007352498-daVlLJwG"
+  liffId: "2007342518-gOdj1LKl",
+  orderDetailBaseUrl: "https://liff.line.me/2007342518-gOdj1LKl",
 };
