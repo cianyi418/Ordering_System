b562d7b367804cdaf2c4523cb90ce5b5260c60bf
cianyi418 <cianyi418@gmail.com>
2025-05-31 16:18:09 +0800

    Update URI in build_order_flex function to use a fixed link and add dummy parameter

--- backend/flex_templates.py
+++ backend/flex_templates.py
@@ -86,7 +86,8 @@ def build_order_flex(order_id, order_items, delivery, total, store_info='', orde
                     "action": {
                         "type": "uri",
                         "label": "查看訂單明細",
-                        "uri": f'{str(safe_uri)}'
+                        "uri": 'https://s.c.s/ffa',
+                        "dummy":"toavoidsemicolon"
                     }
                 },
                 {
